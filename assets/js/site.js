8cadb7caddc6e67dd29853788b537564c1fb6123
Zachariah Arvieux <zacharvieux@gmail.com>
2026-09-13 15:05:02 -0500

    Use refreshed PNG icon assets sitewide

--- assets/js/site.js
+++ assets/js/site.js
@@ -7,3 +7,15 @@ if (navToggle && primaryNav) {
     navToggle.setAttribute('aria-expanded', String(isOpen));
   });
 }
+
+const siteScript = document.currentScript;
+if (siteScript) {
+  const mediaStylesheet = document.createElement('link');
+  mediaStylesheet.rel = 'stylesheet';
+  mediaStylesheet.href = new URL('../css/media-refresh.css', siteScript.src).href;
+  document.head.appendChild(mediaStylesheet);
+}
+
+document.querySelectorAll('img[src*="assets/images/ui/icons/"][src$=".webp"]').forEach((image) => {
+  image.src = image.getAttribute('src').replace(/\.webp$/i, '.png');
+});
